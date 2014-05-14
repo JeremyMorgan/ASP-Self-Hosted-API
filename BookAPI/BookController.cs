adf7248b1e31fc7c6555e2616d4f95c4e1085003
Jeremy Morgan <jeremymorgan@gmail.com>
2014-05-14 03:05:19 -0700

    Initial Commit

---
+++ BookAPI/BookController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace BookAPI
+{
+    public class BookController : ApiController
+    {
+        //public Book[] ourbooks = new Book[]  
+        //{  
+        //    new Book { Id = 1, Title = "Microsoft Visual C# 2012", Author = "John Sharp", Price = 29.99M },  
+        //    new Book { Id = 2, Title = "C# 5.0 in a nutshell", Author = "Joseph Albahari", Price = 19.99M },  
+        //    new Book { Id = 3, Title = "C# in Depth, 3rd Edition", Author = "Jon Skeet", Price = 29.99M },  
+        //    new Book { Id = 4, Title = "Pro ASP.NET MVC 5", Author = "Adam Freeman", Price = 34.01M }
+        //};
+
+        static List<Book> ourbooks = InitBooks();
+
+        private static List<Book> InitBooks()
+        {
+            var booklist = new List<Book>();
+            booklist.Add(new Book { Id = 1, Title = "Microsoft Visual C# 2012", Author = "John Sharp", Price = 29.99M });
+            booklist.Add(new Book { Id = 2, Title = "C# 5.0 in a nutshell", Author = "Joseph Albahari", Price = 19.99M }); 
+            booklist.Add(new Book { Id = 3, Title = "C# in Depth, 3rd Edition", Author = "Jon Skeet", Price = 29.99M });
+            booklist.Add(new Book { Id = 4, Title = "Pro ASP.NET MVC 5", Author = "Adam Freeman", Price = 34.01M });
+
+            return booklist;
+        }
+
+
+        public IEnumerable<Book> Get()
+        {
+            return ourbooks;
+        }
+
+        public Book Get(int Id)
+        {
+
+            var result = (from b in ourbooks
+                          where b.Id == Id
+                          select b).FirstOrDefault();
+
+            if (result == null)
+            {
+                var resp = new HttpResponseMessage(HttpStatusCode.NotFound)
+                {
+                    Content = new StringContent(string.Format("No book with ID = {0}", Id)),
+                    ReasonPhrase = "Book ID Not Found"
+                };
+                throw new HttpResponseException(resp);
+            }
+
+            return result;
+        }
+
+        public void Post([FromBody]Book b)
+        {
+            b.Id = ourbooks.Count + 1;
+            ourbooks.Add(b);
+
+            var resp = new HttpResponseMessage(HttpStatusCode.Created);           
+            throw new HttpResponseException(resp);
+        }
+        public void Put(int Id, [FromBody]Book book)
+        {
+            var result = (from b in ourbooks
+                          where b.Id == Id
+                          select b).FirstOrDefault();
+            
+            result.Title = book.Title;
+            result.Author = book.Author;
+            result.Price = book.Price;
+
+            var resp = new HttpResponseMessage(HttpStatusCode.Accepted);
+            throw new HttpResponseException(resp);
+        }
+
+        public void Delete(int Id)
+        {
+            var result = (from b in ourbooks
+                          where b.Id == Id
+                          select b).FirstOrDefault();
+
+            ourbooks.Remove(result);
+            var resp = new HttpResponseMessage(HttpStatusCode.OK);
+            throw new HttpResponseException(resp);
+        }                   
+
+    }
+}
---
+++ BookAPI/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.SelfHost;
+
+namespace BookAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new HttpSelfHostConfiguration("http://localhost:31337");
+            config.Routes.MapHttpRoute("Default", "api/{controller}/{id}", new { id = RouteParameter.Optional });
+  
+            var server = new HttpSelfHostServer(config);
+
+            var task = server.OpenAsync();
+            task.Wait();
+
+            Console.WriteLine("Server Up");
+            Console.ReadLine();
+
+        }
+    }
+}
